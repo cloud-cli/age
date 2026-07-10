d3239cc30e2e9b2c2f5639f47bf1209f8ed82700
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-07-11 00:35:54 +0530

    feat: add ShellExec

--- src/functions/shell.mjs
+++ src/functions/shell.mjs
@@ -1,9 +1,11 @@
 import { execSync } from "node:child_process";
 
 export function ShellExec(/*string*/ command) {
-  "## Clones a git repository from the specified URL into the target path. ##";
+  "## Runs a shell command in the current workspace ##";
   const options = { cwd: this.getPath("."), encoding: "utf8" };
   return String(execSync(command, options));
 }
 
-export const functions = [ShellExec];
+export const functions = {
+  ShellExec,
+};
