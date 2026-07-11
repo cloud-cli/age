1a73b5869aa51d302593006d70277b48d79af3ed
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-07-11 18:49:05 +0000

    fix git exec unsafe commands

--- src/tools/git.mjs
+++ src/tools/git.mjs
@@ -1,39 +1,31 @@
-import { execSync } from "node:child_process";
+import { exec } from "@cloud-cli/exec";
 
 // functions used by the workspace API via AI function calls to interact with a git repository
-export function CloneRepository(/*string*/ repoUrl, /*string*/ targetPath) {
+export async function CloneRepository(/*string*/ repoUrl, /*string*/ targetPath) {
   "## Clones a git repository from the specified URL into the target path. ##";
   const options = { cwd: this.getPath(".") };
-  execSync(`git clone ${repoUrl} ${targetPath}`, options);
-  return true;
+  const sh = await exec("git", ["clone", repoUrl, targetPath], options);
+  return sh.stdout;
 }
 
-export function CommitChanges(/*string*/ repoPath, /*string*/ message) {
+export async function CommitChanges(/*string*/ repoPath, /*string*/ message) {
   "## Commits all changes in the specified repository path with the given commit message. ##";
   const options = { cwd: this.getPath(".") };
-  execSync(`git -C ${repoPath} add .`, options);
-  execSync(`git -C ${repoPath} commit -m "${message}"`, options);
-  return true;
+  const a = await exec("git", ["-C", repoPath, "add", "."], options);
+  const b = await exec("git", ["-C", repoPath, "commit", "-m", message], options);
+  return a.stdout + "\n" + b.stdout;
 }
 
-export function PushChanges(
-  /*string*/ repoPath,
-  /*string*/ remote = "origin",
-  /*string*/ branch = "main",
-) {
+export async function PushChanges(/*string*/ repoPath, /*string*/ remote = "origin", /*string*/ branch = "main") {
   "## Pushes committed changes from the specified repository path to the given remote and branch. ##";
   const options = { cwd: this.getPath(".") };
-  execSync(`git -C ${repoPath} push ${remote} ${branch}`, options);
-  return true;
+  const sh = await exec("git", ["-C", repoPath, "push", remote, branch], options);
+  return sh.stdout;
 }
 
-export function PullChanges(
-  /*string*/ repoPath,
-  /*string*/ remote = "origin",
-  /*string*/ branch = "main",
-) {
+export async function PullChanges(/*string*/ repoPath, /*string*/ remote = "origin", /*string*/ branch = "main") {
   "## Pulls the latest changes from the specified remote and branch into the given repository path. ##";
   const options = { cwd: this.getPath(".") };
-  execSync(`git -C ${repoPath} pull ${remote} ${branch}`, options);
-  return true;
+  const sh = await exec("git", ["-C", repoPath, "pull", remote, branch], options);
+  return sh.stdout;
 }
